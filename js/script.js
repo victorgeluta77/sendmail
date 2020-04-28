d32e0758159d635e47982f5f4541e71dd901390d
Victor Heliuta <victorgeluta@gmail.com>
2020-04-28 16:34:24 +0300

    add input to form
    add input
    and replay on other mail

--- js/script.js
+++ js/script.js
@@ -4,13 +4,14 @@ const formInput = document.querySelector('.send_form');
      formInput.addEventListener('submit',(e)=>{
          e.preventDefault();
         const template_params = {
-            "topic": e.target[0].value,
+            "name": e.target[0].value,
             "email": e.target[1].value,            
-            "message": e.target[2].value,
+            "phone": e.target[2].value,
+            "message": e.target[3].value,
         }
            console.log(template_params); 
            const service_id = "default_service";
-           const template_id = "my_email_service";
+           const template_id = "czusta_linia_mail_service";
            emailjs.send(service_id, template_id, template_params)
            .then(function(response) {
                if (response.status == 200) alert('Email successfull send !! '+response.status +' '+response.text);
